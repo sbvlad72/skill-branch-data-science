93c6e3c2b4f47b07aaad673a277a5728097737d6
sbvlad72 <sbvlad72@mail.ru>
2021-01-02 23:13:17 +0300

    Add files via upload

--- task_3_solution.py
+++ task_3_solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def split_data_into_two_samples(x):
-    return train_test_split(x, random_state=42)
+    return train_test_split(x, test_size=0.3, random_state=42)
 
 def prepare_data(x):
     #x.drop(columns=["id"], inplace=True)
@@ -13,9 +13,8 @@ def prepare_data(x):
         #print(col)
         x.drop(columns=col, inplace=True)
     #x.drop(columns = [s_dtypes.loc[s_dtypes == 'object'].index], inplace=True)
-    x.reset_index()
-    x.drop(columns=["id"], inplace=True)
-    x.dropna()
-    x1 = x.pop("price_doc")
-    return x, x1
-
+    x.reset_index(drop=True)
+    #x.drop(columns=["id"], inplace=True)
+    x1 = x.dropna()
+    x2 = x1.pop("price_doc")
+    return x1, x2
